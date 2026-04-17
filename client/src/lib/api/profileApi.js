2c2839bd463887986716af7d3d62ed377db83863
mt-intern-tech-4 <intern-tech-4@mindtrot.com>
2026-04-17 13:46:43 +0530

    feat: add uploadAvatar to profileApi

--- client/src/lib/api/profileApi.js
+++ client/src/lib/api/profileApi.js
@@ -4,5 +4,6 @@ export const profileApi = {
   get: () => request('/profile'),
   update: (payload) => request('/profile', { method: 'PATCH', body: payload }),
   updatePreferences: (payload) => request('/profile/preferences', { method: 'PATCH', body: payload }),
+  uploadAvatar: (base64DataUrl) => request('/profile/avatar', { method: 'POST', body: { avatar: base64DataUrl } }),
 };
 
